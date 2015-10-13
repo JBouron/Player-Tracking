4f744158547cfb514300ed27f602312d0dc5031b
Justinien Bouron <justinien.bouron@epfl.ch>
2015-10-13 22:44:24 +0200

    Add class handling coordinate translations.

---
+++ sources/coordinates_translator.cpp
@@ -0,0 +1,13 @@
+#include "../headers/coordinates_translator.h"
+
+namespace tmd{
+    position_t CoordinatesTranslator::world_to_field(position_t w_pos,
+                                                     unsigned char camera_id) {
+        // TODO
+    }
+
+    position_t CoordinatesTranslator::field_to_world(position_t f_pos,
+                                                     unsigned char camera_id) {
+        // TODO
+    }
+}
