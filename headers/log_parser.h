44090ac8e87f22c8921127f35a6047c466f85ada
Justinien Bouron <justinien.bouron@gmail.com>
2015-10-15 01:19:48 +0200

    Implements constructor of the parser.

---
+++ headers/log_parser.h
@@ -0,0 +1,27 @@
+#ifndef BACHELOR_PROJECT_LOG_PARSER_H
+#define BACHELOR_PROJECT_LOG_PARSER_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include "position_t.h"
+
+namespace tmd{
+    /** Class parsing the log containing the player position. */
+    class LogParser{
+    public:
+        LogParser(std::string log_path);
+        ~LogParser();
+
+        std::vector<tmd::position_t> get_next_frame_positions();
+        std::vector<tmd::position_t> get_positions_from_frame(unsigned int frame);
+
+        unsigned int get_current_frame();
+
+    private:
+        std::ifstream m_log;
+        unsigned int m_current_frame;
+    };
+}
+
+#endif //BACHELOR_PROJECT_LOG_PARSER_H
