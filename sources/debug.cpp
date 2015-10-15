58cdcc72df9766cf2cd320c80a48ed5106174386
Justinien Bouron <justinien.bouron@gmail.com>
2015-10-15 17:32:55 +0200

    Log parser is now implemented. Add define to enable debug messages.

--- sources/debug.cpp
+++ sources/debug.cpp
@@ -3,17 +3,21 @@
 namespace tmd {
 
     void debug(std::string message, bool line_return) {
-        std::cout << message;
-        if (line_return) {
-            std::cout << std::endl;
+        if (TMD_DEBUG_ENABLE_DEBUG) {
+            std::cout << message;
+            if (line_return) {
+                std::cout << std::endl;
+            }
         }
     }
 
     void debug(std::string class_name, std::string method_name,
                std::string message, bool line_return) {
-        std::cout << class_name + "::" + method_name + "() : " + message;
-        if (line_return) {
-            std::cout << std::endl;
+        if (TMD_DEBUG_ENABLE_DEBUG) {
+            std::cout << class_name + "::" + method_name + "() : " + message;
+            if (line_return) {
+                std::cout << std::endl;
+            }
         }
     }
 }
