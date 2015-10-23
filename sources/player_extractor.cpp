4454410c9835ed81631f2b9898ab88420c02c0f8
Justinien Bouron <justinien.bouron@epfl.ch>
2015-10-24 01:19:55 +0200

    Fix issues due to changes in frame_t.

--- sources/player_extractor.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/player_extractor.h"
-#include <opencv2/core/core.hpp>
-
-namespace tmd{
-    std::vector<player_t*> PlayerExtractor::extract_player_from_frame(
-            frame_t *frame) {
-        // TODO : implement this !
-    }
-}
