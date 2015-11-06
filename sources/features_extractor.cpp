f5be73bf3956f3d60ec53c39a1cfc815936161e5
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-06 16:34:22 +0100

    Refactorig

--- sources/features_extractor.cpp
+++ sources/features_extractor.cpp
@@ -1,11 +1,10 @@
 #include <opencv2/imgproc/types_c.h>
 #include "../headers/features_extractor.h"
-#include "../headers/player_t.h"
-#include "../headers/features_t.h"
 #include "../headers/debug.h"
 
-namespace tmd{
-    FeaturesExtractor::FeaturesExtractor(std::string model_file){
+namespace tmd {
+
+    FeaturesExtractor::FeaturesExtractor(std::string model_file) {
         m_detector = new DPMDetector(model_file);
         if (m_detector == NULL) {
             throw std::bad_alloc();
@@ -20,15 +19,15 @@ namespace tmd{
             std::vector<player_t *> players) {
         size_t size = players.size();
         size_t i = 0;
-        for (i = 0 ; i < size ; i ++){
+        for (i = 0; i < size; i++) {
             extractFeatures(players[i]);
         }
     }
 
     void FeaturesExtractor::extractFeatures(player_t *player) {
-        if (player == NULL){
+        if (player == NULL) {
             throw std::invalid_argument("Error : Null pointer in "
-                                        "FeaturesExtractor::extractFeatures()");
+                                                "FeaturesExtractor::extractFeatures()");
         }
         extractBodyParts(player);
         convertToHSV(player);
@@ -46,7 +45,7 @@ namespace tmd{
         p->original_image = hsv_tmp;
     }
 
-    static bool isValid(double h, double s, double v){
+    static bool isValid(double h, double s, double v) {
         float th_red_low = TMD_FEATURE_EXTRACTOR_TH_RED_LOW / 2.f;
         float th_red_high = TMD_FEATURE_EXTRACTOR_TH_RED_HIGH / 2.f;
         float th_green_low = TMD_FEATURE_EXTRACTOR_TH_GREEN_LOW / 2.f;
@@ -55,10 +54,11 @@ namespace tmd{
         float th_val_low = TMD_FEATURE_EXTRACTOR_TH_VALUE_LOW;
         tmd::debug("FeaturesExtractor", "isValid", "h = " + std::to_string(h)
                                                    + "s = " + std::to_string(s)
-                                               + "v = " + std::to_string(v));
+                                                   + "v = " +
+                                                   std::to_string(v));
         return ((th_red_low < h && h <= 360) || (0 <= h && h <= th_red_high) ||
-                (th_green_low <= h && h <= th_green_high) ) &&
-                (th_sat_low <= s) && (th_val_low <= v);
+                (th_green_low <= h && h <= th_green_high)) &&
+               (th_sat_low <= s) && (th_val_low <= v);
 
     }
 
@@ -67,13 +67,13 @@ namespace tmd{
         int cols = img.cols;
         int rows = img.rows;
 
-        for (int i = 0 ; i < rows ; i ++){
-            for (int j = 0 ; j < cols ; j ++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 cv::Vec3b color = img.at<cv::Vec3b>(i, j);
-                if (!isValid(color[0], color[1], color[2])){
+                if (!isValid(color[0], color[1], color[2])) {
                     // Need to 'remove' this pixel from the mask.
                     // TODO : Check the following line ...
-                    p->mask_image.at<uchar>(i,j) = 0;
+                    p->mask_image.at<uchar>(i, j) = 0;
                 }
             }
         }
@@ -82,10 +82,10 @@ namespace tmd{
     void FeaturesExtractor::createHistogram(player_t *p) {
         int bins_count = 180;
         int dim = 1; // One dimension : The hue.
-        const int pixel_count = p->original_image.rows*p->original_image.cols;
-        float** range = new float*[1];
+        float **range = new float *[1];
         range[0] = new float[2];
-        range[0][0] = 0; range[0][1] = static_cast<float>(180);
+        range[0][0] = 0;
+        range[0][1] = static_cast<float>(180);
         bool uniform = true;  // Make the histogram uniform.
         bool accumulate = false;
         std::vector<cv::Mat> imagechannels;
@@ -94,12 +94,17 @@ namespace tmd{
         std::vector<cv::Mat> maskchannels;
         cv::split(p->mask_image, maskchannels);
         tmd::debug("FeaturesExtractor", "createHistogram", "p->original_image"
-                ".channels() = " + std::to_string(p->original_image.channels
-                ()));
+                                                                   ".channels() = " +
+                                                           std::to_string(
+                                                                   p->original_image.channels
+                                                                           ()));
         tmd::debug("FeaturesExtractor", "createHistogram", "p->mask_image"""
-           ".channels() = " + std::to_string(p->original_image.channels()));
+                                                                   ".channels() = " +
+                                                           std::to_string(
+                                                                   p->original_image.channels()));
         cv::calcHist(&images[0], 1, 0, maskchannels[0], p->features
-                .color_histogram, dim, &bins_count, (const float**) range,
+                             .color_histogram, dim, &bins_count,
+                     (const float **) range,
                      uniform,
                      accumulate);
     }
