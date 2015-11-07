1d26b8c2df022c60f4a04b862e7c23105105cae6
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-07 23:23:43 +0100

    Add tests for feature comparator

--- headers/test_cases/test_suite.h
+++ headers/test_cases/test_suite.h
@@ -13,6 +13,7 @@ namespace tmd{
 
     void run_tests(void);
 
+    void run_tests_feature_comparator(void);
     void run_tests_bgs(void);
     void run_tests_dpm(void);
     void run_tests_features_extractor(void);
