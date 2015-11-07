44b2bf3dfcbbb88b4fdf46e9b8f08ffba7a5d1ac
Justinien Bouron <justinien.bouron@gmail.com>
2015-11-07 14:09:32 +0100

    features_t : torso is now a Matrix (image) of the torso. Fix missing initialization of torso_mask in dpm-detector.

--- headers/features_t.h
+++ headers/features_t.h
@@ -6,7 +6,7 @@ namespace tmd{
     /* Structure holding the features for one player. */
     typedef struct{
         std::vector<cv::Rect> body_parts;   // Boxes for each body parts.
-        cv::Rect torso;                     // Box for the torso.
+        cv::Mat torso;                     // Box for the torso.
         cv::Mat torso_mask;                 // Mask for the torso.
         cv::Mat torso_color_histogram;      // Color histogram of the torso.
     }features_t;
