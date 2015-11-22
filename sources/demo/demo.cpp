92a415f2c9adb0e187a815285678920c33394d79
Nicolas Roussel <n.roussel13@gmail.com>
2015-11-22 20:33:53 +0100

    Demo clustering functionnal

--- sources/demo/demo.cpp
+++ sources/demo/demo.cpp
@@ -108,8 +108,8 @@ namespace tmd{
             bgs_two_green[i]->jump_to_frame(600);
         }
 
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 2; j++){
+        for(int i = 0; i < 1; i++){
+            for(int j = 0; j < 5; j++){
                 for(int k = 0; k < 10; k++){
                     bgs_two_green[i]->next_frame();
                     bgs_two_red[i]->next_frame();
