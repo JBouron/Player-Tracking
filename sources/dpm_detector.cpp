f5c99d76fc0e4d6c010bbb3a226bec18a8b4847a
Justinien Bouron <justinien.bouron@gmail.com>
2015-11-24 18:34:01 +0100

    DPMDetector : Add a method to resize the parts that are off the boundaries of the player image.

--- sources/dpm_detector.cpp
+++ sources/dpm_detector.cpp
@@ -35,6 +35,8 @@ namespace tmd {
         }
         IplImage playerImage = player->original_image; // Bug in CLion, ignore it.
         player->features.body_parts = getPartBoxesForImage(&playerImage);
+        // Clip the boxes if needed.
+        clipBoxes(player);
         // If there is a player on the image, we compute its torso box.
         if (player->features.body_parts.size() > 0) {
             extractTorsoForPlayer(player);
@@ -109,13 +111,6 @@ namespace tmd {
                 scores[i] > max_score_for_level)
                 max_score_for_level = scores[i];
         }
-
-        const int root_w = filters[0]->sizeX;
-        const int root_h = filters[0]->sizeY;
-        const int root_x = filters[0]->V.x;
-        const int root_y = filters[0]->V.y;
-        cv::Rect root(root_x, root_y, root_w, root_h);
-        parts.push_back(root);
 
         for (i = 0; i < kPoints; i++) {
             for (j = 0; j < n; j++) {
@@ -343,7 +338,7 @@ namespace tmd {
     void DPMDetector::extractTorsoForPlayer(player_t *player) {
         if (player == NULL){
             throw std::invalid_argument("Error null pointer given to "
-                                                "extractTorsoForPlayer method");
+                                            "extractTorsoForPlayer method");
         }
         else if (player->features.body_parts.size() < 3){
             throw std::invalid_argument("Error not enough body parts in "
@@ -361,5 +356,25 @@ namespace tmd {
         player->features.torso = (player->original_image.clone())(mean);
         player->features.torso_mask = (player->mask_image.clone())(mean);
         player->features.torso_pos = mean;
+    }
+
+    void DPMDetector::clipBoxes(player_t* player){
+        size_t boxes_count = player->features.body_parts.size();
+
+        for (size_t i = 0 ; i < boxes_count ; i ++){
+            cv::Rect *part = &(player->features.body_parts[i]);
+
+            // Zero boundaries
+            if (part->x < 0) part->x = 0;
+            if (part->y < 0) part->y = 0;
+
+            // Right and bottom boundaries.
+            if (part->x + part->width > player->original_image.cols){
+                part->width = player->original_image.cols - part->x - 1;
+            }
+            if (part->y + part->height > player->original_image.rows){
+                part->height = player->original_image.rows - part->y - 1;
+            }
+        }
     }
 }
