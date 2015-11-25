0737fcea98dc4867a108c7f77b2cff1bedb7a204
Justinien Bouron <justinien.bouron@epfl.ch>
2015-11-25 19:11:39 +0100

    DPMcalibrator : Add a tool to calibrate the DPMPlayerExtractor.

---
+++ headers/dpm_calibrator.h
@@ -0,0 +1,21 @@
+#ifndef BACHELOR_PROJECT_DPM_CALIBRATOR_H
+#define BACHELOR_PROJECT_DPM_CALIBRATOR_H
+
+#include <opencv2/highgui/highgui.hpp>
+
+namespace tmd{
+
+    /**
+     * Class helping us to determine the effect of each thresholds/parameter
+     * used by the dpm detector.
+     */
+    class DPMCalibrator{
+    public:
+        /**
+         * Launch the calibration on the given video.
+         */
+        static void calibrate_dpm(std::string video_path, int frame_step);
+    };
+}
+
+#endif //BACHELOR_PROJECT_DPM_CALIBRATOR_H
