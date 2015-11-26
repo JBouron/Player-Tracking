68ee49824e6d1ec95279e6b3471532adfc819d4a
Justinien Bouron <justinien.bouron@gmail.com>
2015-11-26 21:13:12 +0100

    team.h : Add helper method returning the team value in string format, or the color value of a team.

--- headers/team_t.h
+++ headers/team_t.h
@@ -1,6 +1,8 @@
 #ifndef TEAM_MEMBERSHIP_DETECTOR_TEAM_T_H
 #define TEAM_MEMBERSHIP_DETECTOR_TEAM_T_H
 
+#include <string>
+
 namespace tmd{
     /* Enumeration for the different teams present on the field. */
     typedef enum{
@@ -9,6 +11,42 @@ namespace tmd{
         TEAM_REFEREE,   // The referee is counted as a team of one person.
         TEAM_UNKNOWN    // Used when the algorithm couldn't determine the team of the player.
     }team_t;
+
+    std::string get_team_string(team_t team){
+        switch (team){
+            case TEAM_A         : return "Team A";
+            case TEAM_B         : return "Team B";
+            case TEAM_REFEREE   : return "Team REFEREE";
+            case TEAM_UNKNOWN   : return "Team UNKNOWN";
+        }
+    }
+
+    CvScalar get_team_color(team_t team){
+        CvScalar color;
+        switch (team){
+            case TEAM_A :
+                color.val[0] = 255;
+                color.val[1] = 0;
+                color.val[2] = 0;
+                color.val[3] = 255;
+                break;
+
+            case TEAM_B :
+                color.val[0] = 0;
+                color.val[1] = 255;
+                color.val[2] = 0;
+                color.val[3] = 255;
+                break;
+
+            case default:
+                color.val[0] = 0;
+                color.val[1] = 0;
+                color.val[2] = 0;
+                color.val[3] = 255;
+                break;
+        }
+        return color;
+    }
 }
 
 #endif //TEAM_MEMBERSHIP_DETECTOR_TEAM_T_H
