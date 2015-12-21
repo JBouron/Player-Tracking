0ce6949ac662f6ba7427a8cf81b2c14eb8dc2865
Nicolas Roussel <n.roussel13@gmail.com>
2015-12-21 14:31:00 +0100

    Optimised bgs improvement

--- sources/bgsubstractor.cpp
+++ sources/bgsubstractor.cpp
@@ -77,14 +77,13 @@ namespace tmd {
         cv::Mat checked_pixels;
         checked_pixels = cv::Mat::zeros(m_static_mask.rows, m_static_mask.cols, CV_8U);
         frame->mask_frame.copyTo(mask_copy);
-        unsigned int buffer_size = 2;
-        unsigned int count_threshold = 0;
+        int buffer_size = 2;
+        int count_threshold = 0;
 
         for (int row = 0; row < m_static_mask.rows; row++) {
             for (int col = 0; col < m_static_mask.cols; col++) {
                 int current_value = frame->mask_frame.at<uchar>(row, col);
                 if (current_value != 0) {
-                    std::cout << "LOL" << std::endl;
                     for (int neighbour_col = -buffer_size; neighbour_col <= buffer_size; neighbour_col++) {
                         for (int neighbour_row = -buffer_size; neighbour_row <= buffer_size; neighbour_row++) {
                             if (col + neighbour_col > 0 && col + neighbour_col < m_static_mask.cols
@@ -92,7 +91,7 @@ namespace tmd {
                                 if (checked_pixels.at<uchar>(row + neighbour_row, col + neighbour_col) == 0) {
                                     if (count_neighbours_in_fg(frame->mask_frame, col + neighbour_col,
                                                                row + neighbour_row, buffer_size) > count_threshold) {
-                                        mask_copy.at<uchar>(row, col) = 255;
+                                        mask_copy.at<uchar>(row + neighbour_row, col + neighbour_col) = 255;
                                     }
                                     checked_pixels.at<uchar>(row + neighbour_row, col + neighbour_col) = 255;
                                 }
@@ -100,19 +99,6 @@ namespace tmd {
                         }
                     }
                 }
-                /*
-                if (current_value != previous_val && current_value == 0) {
-                    if (count_neighbours_in_fg(frame->mask_frame, col, row, buffer_size) > 0) {
-                        mask_copy.at<uchar>(row, col) = 255;
-                    }
-                }
-                else if (current_value != previous_val && current_value != 0) {
-                    if (col > 0 && count_neighbours_in_fg(frame->mask_frame, col - 1, row, buffer_size) > 0) {
-                        mask_copy.at<uchar>(row, col) = 255;
-                    }
-                }
-                previous_val = current_value;
-                */
             }
         }
 
