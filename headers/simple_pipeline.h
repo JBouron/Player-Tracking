97fc8c4f697e24a4161a5984beaf5fe948ec12b6
Justinien Bouron <justinien.bouron@epfl.ch>
2015-12-24 21:52:02 +0100

    SimplePipeline : Apply changes from Pipeline and frame_t.

--- headers/simple_pipeline.h
+++ headers/simple_pipeline.h
@@ -10,16 +10,11 @@ namespace tmd{
     class SimplePipeline : public Pipeline{
 
     public:
-        SimplePipeline(std::string video_path,
-                              std::string model_file,
-                              bool save_frames = false,
-                              std::string output_folder = "");
+        SimplePipeline(std::string video_path, std::string model_file);
 
         ~SimplePipeline();
 
         frame_t* next_frame();
-
-        std::vector<tmd::player_t*> next_players();
 
         /**
          * Sets the properties of the bgs.
@@ -53,13 +48,12 @@ namespace tmd{
          * Extract the players from the given frames, and set their property
          * (team, features, ...)
          */
-        std::vector<tmd::player_t*> extract_players_from_frame
-                (tmd::frame_t* frame);
+        void extract_players_from_frame(tmd::frame_t* frame);
 
-        tmd::BGSubstractor *m_bgSubstractor;
-        tmd::PlayerExtractor *m_playerExtractor;
-        tmd::FeaturesExtractor *m_featuresExtractor;
-        tmd::FeatureComparator *m_featuresComparator;
+        tmd::BGSubstractor      *m_bgSubstractor;
+        tmd::PlayerExtractor    *m_playerExtractor;
+        tmd::FeaturesExtractor  *m_featuresExtractor;
+        tmd::FeatureComparator  *m_featuresComparator;
     };
 }
 
