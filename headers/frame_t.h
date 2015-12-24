615402f4b664b7ed9a1642db04ff2330fa55f9bc
Justinien Bouron <justinien.bouron@epfl.ch>
2015-12-24 21:45:22 +0100

    Move static method get_colored_mask.

--- headers/frame_t.h
+++ headers/frame_t.h
@@ -7,6 +7,7 @@
 #include "opencv2/video/background_segm.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "player_t.h"
+#include "features_t.h"
 
 namespace tmd {
 
@@ -30,6 +31,78 @@ namespace tmd {
             free_player(frame->players[i]);
         }
         delete frame;
+    }
+
+    /**
+     * Create a 'colored mask' ie all pixel belonging to the foreground
+     * are in color whereas pixels from the background are black.
+     */
+    cv::Mat get_colored_mask_for_frame(tmd::frame_t *frame) {
+        cv::Mat resulting_image;
+        frame->original_frame.copyTo(resulting_image);
+        cv::Vec3b black;
+        black.val[0] = 0;
+        black.val[1] = 0;
+        black.val[2] = 0;
+        for (int c = 0; c < frame->mask_frame.cols; c++) {
+            for (int r = 0; r < frame->mask_frame.rows; r++) {
+                if (frame->mask_frame.at<uchar>(r, c) < 127) {
+                    resulting_image.at<cv::Vec3b>(r, c) = black;
+                }
+            }
+        }
+        return resulting_image;
+    }
+
+    /**
+     * Draw the players of the frame on another image and returns it.
+     */
+    cv::Mat draw_player_on_frame(tmd::frame_t *frame, bool draw_torso = false,
+                                 bool draw_parts = false){
+        cv::Mat result = frame->original_frame;
+
+        std::vector<tmd::player_t*> &players = frame->players;
+
+        CvScalar torso_color;
+        torso_color.val[0] = 255;
+        torso_color.val[1] = 255;
+        torso_color.val[2] = 0;
+        torso_color.val[3] = 255;
+        CvScalar body_part_color;
+        body_part_color.val[0] = 255;
+        body_part_color.val[1] = 0;
+        body_part_color.val[2] = 255;
+        body_part_color.val[3] = 255;
+
+        const int thickness = 1; // Thickness of the box.
+        const int line_type = 8; // 8 connected line.
+        const int shift = 0;
+
+        for (player_t* p : players){
+            // Due to overlapping we draw first the body parts, then the
+            // torso and finally th box around the player.
+            if (draw_parts){
+                for (cv::Rect part : p->features.body_parts){
+                    cv::Rect pos = part;
+                    pos.x += p->pos_frame.x;
+                    pos.y += p->pos_frame.y;
+                    cv::rectangle(result, pos, body_part_color, thickness,
+                                  line_type, shift);
+                }
+            }
+
+            if (draw_torso){
+                cv::Rect pos = p->features.torso_pos;
+                pos.x += p->pos_frame.x;
+                pos.y += p->pos_frame.y;
+                cv::rectangle(result, pos, torso_color,
+                              thickness, line_type, shift);
+            }
+
+            cv::rectangle(result, p->pos_frame, tmd::get_team_color(p->team),
+                          thickness, line_type, shift);
+        }
+        return result;
     }
 }
 
