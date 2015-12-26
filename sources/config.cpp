629959397cc4c41691edfb49c74608b5c22870de
Justinien Bouron <justinien.bouron@epfl.ch>
2015-12-26 14:15:34 +0100

    BGSubstractor : Now uses config class.

--- sources/config.cpp
+++ sources/config.cpp
@@ -2,4 +2,7 @@
 
 namespace tmd{
     bool Config::bgs_detect_shadows = false;
+    float Config::bgs_threshold = 256;
+    int Config::bgs_history = 500;
+    float Config::bgs_learning_rate = 0.0;
 }
