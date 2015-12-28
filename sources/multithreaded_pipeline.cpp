15bca25b9f02fcbeffe582ce0064e07e4d58b568
Justinien Bouron <justinien.bouron@epfl.ch>
2015-12-28 15:14:19 +0100

    MultithreadedPipeline : Add the schedule_threads method to create the threads and schedule them. Solve issue with wrong ending frames for threads.

--- sources/multithreaded_pipeline.cpp
+++ sources/multithreaded_pipeline.cpp
@@ -15,31 +15,7 @@ namespace tmd{
         m_next_thread_to_use = 0;
         m_pipeline_threads = new tmd::PipelineThread*[m_thread_count];
 
-        /* Create threads */
-        tmd::debug("MultithreadedPipeline", "create_threads", "Creating "
-                "threads");
-        int mod = m_end % m_thread_count;
-        for (int i = 0 ; i < m_thread_count ; i ++){
-            int threadId = i;
-            int starting_frame = m_start + threadId*m_step;
-            int ending_frame;
-            if (threadId < mod){
-                ending_frame = threadId - mod + m_end;
-            }
-            else{
-                ending_frame = threadId - mod + m_end + m_thread_count;
-            }
-            int step = m_step * m_thread_count;
-            tmd::debug("MultithreadedPipeline", "create_threads", "Creating "
-            "thread " + std::to_string(i) + " starting_frame = " +
-            std::to_string(starting_frame) + " ending_frame = " +
-            std::to_string(ending_frame) + " step = "  + std::to_string(step));
-            m_pipeline_threads[i] = new PipelineThread(video_folder,
-                                                       m_camera_index,
-                                                       threadId,
-                                                       starting_frame,
-                                                       ending_frame, step);
-        }
+        schedule_threads(video_folder);
     }
 
     MultithreadedPipeline::~MultithreadedPipeline() {
@@ -55,5 +31,35 @@ namespace tmd{
         m_frame_pos += m_step;
         m_next_thread_to_use = (m_next_thread_to_use + 1)%m_thread_count;
         return frame;
+    }
+
+    void MultithreadedPipeline::schedule_threads(std::string video_folder){
+/* Create threads */
+        tmd::debug("MultithreadedPipeline", "create_threads", "Creating "
+                "threads");
+        m_end = m_end - (m_end % m_step);
+        int mod = (m_end / m_thread_count) % m_thread_count;
+        for (int i = 0 ; i < m_thread_count ; i ++){
+            int threadId = i;
+            int starting_frame = m_start + threadId*m_step;
+            int ending_frame;
+            if (threadId <= mod){
+                ending_frame = m_end - (mod - threadId) * m_step;
+            }
+            else{
+                ending_frame = m_end - (mod - threadId) * m_step -
+                        m_thread_count*m_step;
+            }
+            int step = m_step * m_thread_count;
+            tmd::debug("MultithreadedPipeline", "create_threads", "Creating "
+                                                                          "thread " + std::to_string(i) + " starting_frame = " +
+                                                                  std::to_string(starting_frame) + " ending_frame = " +
+                                                                  std::to_string(ending_frame) + " step = "  + std::to_string(step));
+            m_pipeline_threads[i] = new PipelineThread(video_folder,
+                                                       m_camera_index,
+                                                       threadId,
+                                                       starting_frame,
+                                                       ending_frame, step);
+        }
     }
 }
